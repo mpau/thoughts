f0d64a1912ba76ebc2904e829ff16db2c29be95f
Michael Pauli <m.pauli@gmx.info>
2025-09-15 14:30:53 +0200

    test: Add tests

--- feature_perspective_screen/src/test/java/com/mpauli/feature/perspectivescreen/viewmodel/PerspectiveScreenStateProcessorTest.kt
+++ feature_perspective_screen/src/test/java/com/mpauli/feature/perspectivescreen/viewmodel/PerspectiveScreenStateProcessorTest.kt
@@ -2,12 +2,49 @@ package com.mpauli.feature.perspectivescreen.viewmodel
 
 import com.mpauli.feature.perspectivescreen.domain.model.ApodItemState
 import com.mpauli.feature.perspectivescreen.viewmodel.model.ViewAction
+import com.mpauli.feature.perspectivescreen.viewmodel.model.ViewState
 import kotlinx.coroutines.test.runTest
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 internal class PerspectiveScreenStateProcessorTest {
+
+    @Test
+    fun `should process ViewAction#ShowLoading`() = runTest {
+        // Given
+        val viewAction = ViewAction.ShowLoading
+
+        val perspectiveScreenStateProcessor = PerspectiveScreenStateProcessor()
+
+        // When
+        val resultState = perspectiveScreenStateProcessor.process(viewAction)
+
+        // Then
+        resultState shouldBeEqualTo ViewState.DEFAULT.copy(
+            apodItemState = ViewState.DEFAULT.apodItemState,
+            isLoading = true,
+            isError = false
+        )
+    }
+
+    @Test
+    fun `should process ViewAction#ShowError`() = runTest {
+        // Given
+        val viewAction = ViewAction.ShowError
+
+        val perspectiveScreenStateProcessor = PerspectiveScreenStateProcessor()
+
+        // When
+        val resultState = perspectiveScreenStateProcessor.process(viewAction)
+
+        // Then
+        resultState shouldBeEqualTo ViewState.DEFAULT.copy(
+            apodItemState = ViewState.DEFAULT.apodItemState,
+            isLoading = false,
+            isError = true
+        )
+    }
 
     @Test
     fun `should process ViewAction#UpdateApod`() = runTest {
@@ -28,6 +65,10 @@ internal class PerspectiveScreenStateProcessorTest {
         val resultState = perspectiveScreenStateProcessor.process(viewAction)
 
         // Then
-        resultState.apodItemState shouldBeEqualTo apodItemState
+        resultState shouldBeEqualTo ViewState(
+            apodItemState = apodItemState,
+            isLoading = false,
+            isError = false
+        )
     }
 }
